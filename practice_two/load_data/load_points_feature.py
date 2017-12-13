64a464044520c229cd9edd406c2c8787295a8512
Dxq <chk0125@126.com>
2017-12-13 14:33:03 +0800

    read_point

--- practice_two/load_data/load_points_feature.py
+++ practice_two/load_data/load_points_feature.py
@@ -15,21 +15,21 @@ def main():
     files = os.listdir(logdir)
     num = len(files)
     print('total num ------>', num)
-    data_X = np.zeros((num, 71 * 2))
+    data_X = np.zeros((num, 72 * 2))
     data_Y = np.zeros((num, 9))
     for i, file in enumerate(files):
         print('read_{}_data------->loading----->start'.format(file))
         points = scio.loadmat(logdir + '/' + file)['Points']
-        p2v = points[1:] - points[:-1]
+        # p2v = points[1:] - points[:-1]
 
-        data_X[i, :] = np.array(p2v).reshape(1, -1)
+        data_X[i, :] = np.array(points).reshape(1, -1)
         data_Y[i, :] = scio.loadmat(label_dir + '/' + file.replace('Point', 'Label'))['Label']
         print('read_{}_data------->loading----->end'.format(file))
 
-    scio.savemat('face_1_channel_XY_Points', {"X": data_X, "Y": data_Y})
+    scio.savemat('face_1_channel_XY_Points72', {"X": data_X, "Y": data_Y})
 
 
 if __name__ == '__main__':
-    tt = scio.loadmat('face_1_channel_XY_Points.mat')
+    tt = scio.loadmat('face_1_channel_XY_Points72.mat')
     print(tt['X'].shape)
     # main()
