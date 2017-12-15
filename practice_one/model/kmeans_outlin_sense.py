5d49cf9541194a92ada1b8390d82c5814829a807
sunyihuan <syh2685@126.com>
2017-12-15 17:35:26 +0800

    kmeans . forest

--- practice_one/model/kmeans_outlin_sense.py
+++ practice_one/model/kmeans_outlin_sense.py
@@ -33,7 +33,7 @@ def main(Xtr, Ytr, Xte, Yte):
                     use_mini_batch=True)
 
     res = kmeans.training_graph()
-    (all_scores, cluster_idx, scores, cluster_centers_initialized, init_op,
+    (all_scores, cluster_idx, scores, cluster_centers_initialized, cluster_centers_var, init_op,
      train_op) = kmeans.training_graph()
     cluster_idx = cluster_idx[0]  # fix for cluster_idx being a tuple
     avg_distance = tf.reduce_mean(scores)
@@ -57,6 +57,7 @@ def main(Xtr, Ytr, Xte, Yte):
         summary_write, _, d, idx = sess.run(
             [merge_all_op, train_op, avg_distance, cluster_idx],
             feed_dict={X: Xtr})
+
         if i % 10 == 0 or i == 1:
             write.add_summary(summary_write, i)
             print("Step %i, Avg Distance: %f" % (i, d))
@@ -81,19 +82,13 @@ def main(Xtr, Ytr, Xte, Yte):
 
     # Test Model
     test_x, test_y = Xte, Yte
-    Y, cluster_label, correct_prediction, accuracy_op = sess.run(
-        [tf.argmax(Y, 1), cluster_label, correct_prediction, accuracy_op],
-        feed_dict={X: test_x, Y: test_y})
-    print("Test Accuracy:", accuracy_op)
+    with sess.as_default():
+        accuracy_train_op = accuracy_op.eval(feed_dict={X: Xtr, Y: Ytr})
+        accuracy_test_op = accuracy_op.eval(feed_dict={X: test_x, Y: test_y})
+        cluster_label = cluster_label.eval(feed_dict={X: Xtr, Y: Ytr})
+        print("Train Accuracy:", accuracy_train_op)
+        print("Test Accuracy:", accuracy_test_op)
     # print('correct_prediction', correct_prediction)
-
-    # 计算有多少小判断成大
-    c = 0
-    for i in range(len(correct_prediction)):
-        if correct_prediction[i] == False:
-            if abs(cluster_label[i] - Y[i]) > 1:
-                c += 1
-    print(round(100 * (c / test_y.shape[0]), 2), "%")
 
     return cluster_label
 
@@ -111,9 +106,9 @@ def load_data_train(file):
 
 num_steps = 30  # Total steps to train
 batch_size = 1024  # The number of samples per batch
-k = 22  # The number of clusters
+k = 500  # The number of clusters
 num_classes = 3  # The 10 digits
-num_features = 64 * 64  # Each image is 28x28 pixels
+num_features = 28  # Each image is 28x28 pixels
 
 if __name__ == '__main__':
 
@@ -122,7 +117,7 @@ if __name__ == '__main__':
         file = 'F:/dataSets/FaceChannel1/face_1_channel_XY'
     elif name == 'Syh':
         file_sense = 'E:/deeplearning_Data/face_1_channel_sense_XY64'
-        file_outlin = 'E:/deeplearning_Data/face_1_channel_outlin_XY64'
+        file_outlin = 'face_1_channel_sense.mat'
 
     # 样本比例检测
     # data_check(Y_test)
@@ -131,15 +126,19 @@ if __name__ == '__main__':
     data_train = scio.loadmat(file_outlin)
 
     X_train0, X_test0, Y_train0, Y_test0 = train_test_split(data_train['X'], data_train['Y'], test_size=0.2)
-    X_train0 = X_train0.reshape(-1, X_train0.shape[1] * X_train0.shape[2])
-    X_test0 = X_test0.reshape(-1, X_test0.shape[1] * X_test0.shape[2])
+    print(X_train0.shape)
+    # X_train0 = X_train0.reshape(-1, X_train0.shape[1] * X_train0.shape[2])
+
+    # X_test0 = X_test0.reshape(-1, X_test0.shape[1] * X_test0.shape[2])
     cluster_sense = main(X_train0, Y_train0, X_test0, Y_test0)
+    for i in range(3):
+        print(str(i) + '的比例', round(100.0 * list(cluster_sense).count(i) / len(cluster_sense), 2), '%')
+    # 计算有多少小判断成大
+    c = 0
+    for i in range(len(cluster_sense)):
+        if abs(cluster_sense[i] - np.argmax(Y_train0[i])) > 1:
+            c = c + 1
+    print(round(100 * (c / len(cluster_sense)), 2), "%")
 
-    cluster_sense = pd.DataFrame(cluster_sense)
-    cluster_sense.to_csv('cluster_outlin.csv')
-
-    # Y = scio.loadmat('E:/deeplearning_Data/face_1_channel_XY')
-    # Y = Y['Y'][1200:, :]
-    # correct_prediction = tf.equal(cluster_labels, tf.cast(tf.argmax(Y, 1), tf.int32))
-    # # print(correct_prediction)
-    # accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # cluster_sense = pd.DataFrame(cluster_sense)
+    # cluster_sense.to_csv('cluster_outlin.csv')
