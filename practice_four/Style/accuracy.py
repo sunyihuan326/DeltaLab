07b0af2eed3768db1f82a8c3df0e21a7f47948e7
Dxq <chk0125@126.com>
2018-01-05 11:35:28 +0800

    march

--- practice_four/Style/accuracy.py
+++ practice_four/Style/accuracy.py
@@ -4,3 +4,69 @@ Created on 2018/1/4.
 
 @author: chk01
 '''
+from sklearn.metrics import confusion_matrix
+from practice_four.utils import *
+from collections import Counter
+
+outline_parameters = scio.loadmat('parameter/outline64x64_parameter-2500.mat')
+sense_parameters = scio.loadmat('parameter/sense64_parameter-3500.mat')
+
+
+def get_outline64(trX):
+    W = outline_parameters['W1']
+    b = outline_parameters['b1']
+    Z = np.add(np.matmul(trX, W.T), b)
+    return np.squeeze(np.argmax(Z, 1))
+
+
+def get_sense64(trX):
+    W = sense_parameters['W1']
+    b = sense_parameters['b1']
+    Z = np.add(np.matmul(trX, W.T), b)
+    return np.squeeze(np.argmax(Z, 1))
+
+
+def main():
+    data = scio.loadmat('data/style64x64.mat')
+    trX = data['X']
+    trY = np.argmax(data['Y'], 1)
+    m, _ = trX.shape
+    outline = get_outline64(trX)
+    sense = get_sense64(trX)
+    style = 3 * outline + sense
+
+    train_res_matrix = confusion_matrix(y_true=trY, y_pred=style)
+    correct = 0
+    error = 0
+    for i in range(m):
+        if style[i] in accept_ans[trY[i]]:
+            correct += 1
+        elif style[i] in absolute_error[trY[i]]:
+            error += 1
+
+    print('准确率：', round(train_res_matrix.trace() / m, 2))
+    print('可接受率：', round(correct / m, 2))
+    print('原则性错误率：', round(error / m, 2))
+    for i in range(9):
+        num = np.sum(train_res_matrix[i, :])
+        acc = round((train_res_matrix[i, i]) / num, 2)
+        accept_num = 0
+        err_num = 0
+        for j in accept_ans[i]:
+            accept_num += train_res_matrix[i, j]
+        for k in absolute_error[i]:
+            err_num += train_res_matrix[i, k]
+        accept = round(accept_num / num, 2)
+        err = round(err_num / num, 2)
+        print('输入--------', i, '--------------')
+        print('准确率：', acc, '|可接受率：', accept, '|原则性错误率：', err)
+        print('----------------------------------------------------')
+    # c = Counter(style)
+    # c.most_common()
+    # print(c)
+
+    return True
+
+
+if __name__ == '__main__':
+    main()
---
+++ practice_four/Style/load_data.py
@@ -0,0 +1,67 @@
+# coding:utf-8
+'''
+Created on 2018/1/5.
+
+@author: chk01
+'''
+import os
+from PIL import Image, ImageDraw, ImageEnhance
+from practice_four.utils import *
+
+
+def get_face_box(points):
+    X = points[:, 0]
+    Y = points[:, 1]
+    min_x = min(X) - 10
+    max_x = max(X) + 10
+    min_y = min(Y) - 10
+    max_y = max(Y) + 10
+
+    wid = max(max_y - min_y, max_x - min_x)
+
+    new_x = min_x - (wid - (max_x - min_x)) // 2
+    new_y = min_y - (wid - (max_y - min_y)) // 2
+
+    pil_image = Image.new("RGB", (2000, 2000), color=255)
+    d = ImageDraw.Draw(pil_image)
+
+    d.line([tuple(p) for p in points[:13]], width=10, fill=0)
+    d.line([tuple(p) for p in points[13:21]], width=10, fill=0)
+    d.line([tuple(p) for p in [points[13], points[20]]], width=10, fill=0)
+    d.line([tuple(p) for p in points[30:38]], width=10, fill=0)
+    d.line([tuple(p) for p in [points[30], points[37]]], width=10, fill=0)
+    d.line([tuple(p) for p in points[22:30]], width=10, fill=0)
+    d.line([tuple(p) for p in [points[22], points[29]]], width=10, fill=0)
+    d.line([tuple(p) for p in points[39:47]], width=10, fill=0)
+    d.line([tuple(p) for p in [points[39], points[46]]], width=10, fill=0)
+    d.line([tuple(p) for p in points[47:57]], width=10, fill=0)
+    d.line([tuple(p) for p in points[58:66]], width=10, fill=0)
+    d.line([tuple(p) for p in [points[58], points[65]]], width=10, fill=0)
+
+    region = pil_image.crop([new_x, new_y, new_x + wid, new_y + wid])
+    region = region.resize((64, 64), Image.ANTIALIAS).convert("L")
+    region = ImageEnhance.Contrast(region).enhance(999)
+    return region
+
+
+def main():
+    logdir = '../../practice_two/data/image3channel'
+    label_dir = '../../practice_two/data/label'
+    files = os.listdir(logdir)
+    num = len(files)
+    print('total num ------>', num)
+    data_X = np.zeros((num, 64 * 64))
+    data_Y = np.zeros((num, 9))
+    for i, file in enumerate(files):
+        print('read_{}_data------->loading----->start'.format(file))
+        points = scio.loadmat(logdir + '/' + file)['Points']
+        data_X[i, :] = np.array(get_face_box(points)).reshape(1, -1)
+        data_Y[i, :] = scio.loadmat(label_dir + '/' + file.replace('Point', 'Label'))['Label']
+
+        print('read_{}_data------->loading----->end'.format(file))
+
+    scio.savemat('data/style64x64', {"X": data_X, "Y": data_Y})
+
+
+if __name__ == '__main__':
+    main()
