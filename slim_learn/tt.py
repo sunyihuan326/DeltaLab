387d0ed597a0ecc9d24fcf1b9f2629e8775e792a
sunyihuan <syh2685@126.com>
2018-05-28 18:13:35 +0800

    slim_learn

---
+++ slim_learn/tt.py
@@ -0,0 +1,15 @@
+# coding:utf-8 
+'''
+created on 2018/5/28
+
+@author:sunyihuan
+'''
+from tensorflow.contrib.slim.nets import *
+
+alexnet
+inception
+overfeat
+resnet_utils
+resnet_v1
+resnet_v2
+vgg
