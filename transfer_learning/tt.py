6ae6bf6f023275ae7b7a62fc53ff1be0abb814b9
sunyihuan <syh2685@126.com>
2018-09-18 20:43:26 +0800

    color

--- transfer_learning/tt.py
+++ transfer_learning/tt.py
@@ -7,6 +7,7 @@ created on 2018/8/31
 import os
 from tensorflow.python import pywrap_tensorflow
 import tensorflow as tf
+import numpy as np
 
 # hair_length_graph = tf.Graph()
 #
@@ -23,6 +24,45 @@ model_path = "/Users/sunyihuan/Desktop/parameters/hair_length/second/complex77_s
 #     print("tensor_name: ", key)
 #     print(sess.get_tensor(key))
 
-from tensorflow.python.tools import inspect_checkpoint as chkp
+# 查看ckpt中的tensor
+# from tensorflow.python.tools import inspect_checkpoint as chkp
 
-chkp.print_tensors_in_checkpoint_file(file_name=model_path, tensor_name='', all_tensors=False, all_tensor_names=True)
+# chkp.print_tensors_in_checkpoint_file(file_name=model_path, tensor_name='', all_tensors=False, all_tensor_names=True)
+
+a = tf.constant(3.0, dtype=tf.float32)
+b = tf.constant(4.0)
+total = a + b
+# print(a)
+# print(b)
+# print(total)
+sess = tf.Session()
+# print(sess.run(total))
+x = tf.placeholder(tf.float32, shape=[None, 3])
+linear_model = tf.layers.Dense(units=1)
+print(linear_model)
+y = linear_model(x)
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(y, {x: [[1, 2, 3],[4, 5, 6],[7,8,9]]}))
+
+features = {
+    'sales' : [[5], [10], [8], [9]],
+    'department': ['sports', 'sports', 'gardening', 'gardening']}
+
+department_column = tf.feature_column.categorical_column_with_vocabulary_list(
+        'department', ['sports', 'gardening'])
+department_column = tf.feature_column.indicator_column(department_column)
+
+columns = [
+    tf.feature_column.numeric_column('sales'),
+    department_column
+]
+
+inputs = tf.feature_column.input_layer(features, columns)
+var_init = tf.global_variables_initializer()
+table_init = tf.tables_initializer()
+sess = tf.Session()
+sess.run((var_init, table_init))
+print(sess.run(inputs))
+
+
